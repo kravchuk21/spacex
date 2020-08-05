20076a2b1c18a7bbc030f14157a9bb2250663d5a
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2020-08-05 12:04:24 +0300

    section world, rocket

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let instance = new vidbg(".video", {
+  mp4: "video/world.mp4",
+  webm: "video/world.webm",
+  poster: "video/poster.jpg",
+  overlay: false,
+});
+
+let rellax = new Rellax('.rocket');
