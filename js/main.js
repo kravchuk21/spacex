bf220f0f1313e64fca3d9bc8bad470f6713305a5
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2020-08-07 11:03:28 +0300

    animate

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ let instance = new vidbg(".video", {
   overlay: false,
 });
 
-let rellax = new Rellax('.rocket');
-if(document.body.clientWidth < 576){
-  rellax.destroy()
+let rellax = new Rellax(".rocket");
+if (document.body.clientWidth < 576) {
+  rellax.destroy();
 }
+AOS.init();
